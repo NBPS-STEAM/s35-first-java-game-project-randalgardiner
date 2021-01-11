208c97c9a999e7dea3b946d49a99139ae327cabe
Randal Gardiner <randal.gardiner@mynbps.org>
2021-01-11 02:30:19 +0000

    I then created a while statement and encapsulated the following if and if else statements into these brackets to form a loop. I used a while loop instead of a for loop because I wanted the user to be to have as many unlimited guesses until they got it right. Once it is correct, the loop finishes and the game is over.

--- GameRandomizer.java
+++ GameRandomizer.java
@@ -13,6 +13,26 @@ public class GameRandomizer {
         computerNumber = rand.nextInt(101);
         boolean win = false;
 
+         while(userNumber != computerNumber){
+        System.out.print("Guess a number between 1 and 100: ");
+        userNumber = scan.nextInt();
+        
+        if(userNumber == computerNumber){
+        win = true;
+        System.out.println("Correct!");
+        }
+        
+        else if(userNumber > computerNumber){
+        System.out.println("The number you have guessed is too high!"); 
+        }
+        
+        else if(userNumber < computerNumber){
+        System.out.println("The number you have guessed is too low!");
+        }
 
     }
+    
+    }
+    
 }
+
