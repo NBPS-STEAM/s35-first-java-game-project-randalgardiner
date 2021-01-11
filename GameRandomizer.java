0a86b4d06618e02569cae3c3c5ac2c45328727ec
Randal Gardiner <randal.gardiner@mynbps.org>
2021-01-11 02:25:34 +0000

    I assigned the variable computerNumber to the number that the computer is going to generate
    I assigned the variable userNumber to the number that the user will guess. I set it to 0 because the user has not guessed anything yet.
    
    I created an object for the scanner class and an object for the random class
    
    We set the computerNumber to generate a random number. I placed the number as 101 so that the range was from 1 - 100

--- GameRandomizer.java
+++ GameRandomizer.java
@@ -3,6 +3,16 @@ import java.util.Random;
 
 public class GameRandomizer {
     public static void main(String[] args) {
-        
+
+         int computerNumber;
+         int userNumber = 0;
+         
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        computerNumber = rand.nextInt(101);
+        boolean win = false;
+
+
     }
 }
